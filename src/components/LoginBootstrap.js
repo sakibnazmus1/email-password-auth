405be3496963eb5589583fc3d09f5862f2a58804
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-07 19:00:24 +0600

    secound commit

--- src/components/LoginBootstrap.js
+++ src/components/LoginBootstrap.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase/firebase.init";
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 const LoginBootstrap = () => {
+    const [success, setSuccess] = useState(false)
     const handleSubmit = event => {
         event.preventDefault();
-
+        setSuccess(false)
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
@@ -17,6 +19,7 @@ const LoginBootstrap = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                setSuccess(true)
             })
             .catch(error => {
                 console.error('error', error)
@@ -38,6 +41,7 @@ const LoginBootstrap = () => {
                 </div>
                 <button className="btn btn-primary" type="submit">Login</button>
             </form>
+            {success && <p>Succesfully login to the account</p>}
             <br />
             <p><small>New to this Website? Please  <Link to='/register'>Register</Link></small></p>
         </div>
